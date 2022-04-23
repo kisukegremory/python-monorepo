87130106f36a43e10152ac5677c80b722bc0617d
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2022-04-23 14:38:56 -0300

    feat: benchmark using the levensthein distance

--- projects/email-text-similarity/main.py
+++ projects/email-text-similarity/main.py
@@ -1,4 +1,28 @@
+from ctypes import Union
+from typing import List
+from src.config import MOST_EMAIL_CASES
 import textdistance
+
+
+class EmailSuggestion:
+    def __init__(self, email: str) -> None:
+        self.email = email
+
+    def fit(
+        self,
+        distance_function = textdistance.hamming.normalized_similarity,
+        patterns: List[str] = MOST_EMAIL_CASES
+    ):
+
+        self.similarities = {
+            pattern: distance_function(pattern,self.email)
+            for pattern in patterns
+        }
+    
+    def suggest(self):
+        sims = list(self.similarities.values())
+        if max(sims) > 0.7:
+            return list(self.similarities.keys())[sims.index(max(sims))]
 
 
 test_list = ["gamail.com",
@@ -22,8 +46,30 @@ test_list = ["gamail.com",
 "gml.com",
 "gmsil.com",
 "gnail.com",
-"hmail.com"]
+"hmail.com",
+"htmail.com",
+"hotmsil.com",
+"hotmil.com",
+"hotmal.com",
+"hotmaim.com",
+"hotmail.con",
+"hotmail.com.com",
+"hotmail.com.br",
+"hotmail.com",
+"hotmail.co",
+"hotmai.com",
+"hotamail.com",
+"hormail.com",
+"homail.com",
+"outlook.vom",
+"outlook.pt",
+"outlook.com.br",
+"outlook.com",
+"outloo.com",
+"outllok.com",
+]
 
 for email in test_list:
-    calc = textdistance.hamming.normalized_similarity('gmail.com', email)
-    print(f"{email}: {calc >= 0.7} - {calc:.2f}")
+    suggestion = EmailSuggestion(email)
+    suggestion.fit(textdistance.DamerauLevenshtein().normalized_similarity)
+    print(f"{email} - {suggestion.suggest()} - {suggestion.similarities}")
---
+++ projects/email-text-similarity/src/config.py
@@ -0,0 +1,1 @@
+MOST_EMAIL_CASES = ['gmail.com', 'outlook.com', 'hotmail.com']
